949219adf8398d6d0908c4cd6d93d44e1cb9fde5
1BBC <bbc21249@gmail.com>
2017-09-04 10:14:46 +0300

    add lang files

---
+++ lng_english.pl
@@ -0,0 +1,23 @@
+$lang{TIMETRACKER} = "Time tracking";
+$lang{FILLING_TIMETRACKER} = "Filling in the accounting of time";
+
+$lang{SUPPORT_TIMETRACK} = "Support";
+$lang{SPRINT_TIMETRACK} = "Sprint";
+$lang{COEFFICIENT} = "Focus factor,%";
+
+$lang{SUBMIT_BTN_ADD_TIMETRACK} = "Add";
+$lang{SUBMIT_BTN_EDIT_TIMETRACK} = "Edit";
+
+
+$lang{ADD_MASSAGE} = "Add";
+$lang{DELETE_MASSAGE} = "Delete";
+$lang{CHANGE_MASSAGE} = "Change";
+$lang{OPERATION} = "Operation completed successfully";
+
+$lang{ADD_ITEM} = "Add item";
+$lang{EDIT_ITEM} = "Edit Item";
+$lang{NEW_ITEM} = "New item";
+
+$lang{PRIORITY} = "Priority";
+$lang{ELEMENTS} = "Elements";
+$lang{ELEMENT} = "Element";
---
+++ lng_ukrainian.pl
@@ -0,0 +1,23 @@
+$lang{TIMETRACKER} = "Облік часу";
+$lang{FILLING_TIMETRACKER} = "Заповнення обліку часу";
+
+$lang{SUPPORT_TIMETRACK} = "Саппорт";
+$lang{SPRINT_TIMETRACK} = "Спринт";
+$lang{COEFFICIENT} = "Фокус фактор,%";
+
+$lang{SUBMIT_BTN_ADD_TIMETRACK} = "Додати";
+$lang{SUBMIT_BTN_EDIT_TIMETRACK} = "Змінити";
+
+
+$lang{ADD_MASSAGE} = "Додати";
+$lang{DELETE_MASSAGE} = "Видалити";
+$lang{CHANGE_MASSAGE} = "Зміна";
+$lang{OPERATION} = "Операція успішно виконана";
+
+$lang{ADD_ITEM} = "Додати елемент";
+$lang{EDIT_ITEM} = "Змінити елемент";
+$lang{NEW_ITEM} = "Новий елемент";
+
+$lang{PRIORITY} = "Пріоритет";
+$lang{ELEMENTS} = "Елементи";
+$lang{ELEMENT} = "Елемент";
